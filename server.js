4275babb0c205ae0dcfc6912f67bcfd03cde6fbe
Danish Khan <khandanish78866@gmail.com>
2024-11-07 13:47:23 +0530

    new cors

--- server.js
+++ server.js
@@ -17,24 +17,9 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-// app.use(cors({
-//     origin: 'http://localhost:3000', 
-//     credentials: true
-// }));
-
-const allowedOrigins = ['http://localhost:3000', 'https://nuxtbazaar.vercel.app/'];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'https://nuxtbazaar.vercel.app', 
     credentials: true
 }));
 
